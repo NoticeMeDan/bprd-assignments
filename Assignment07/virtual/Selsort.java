7f54485ffa9efe68897987bb21283c3b83383875
Elias Jørgensen <eliaszakarias@gmail.com>
2019-11-01 14:21:14 +0100

    Analyse java bytecode

--- Assignment07/virtual/Selsort.java
+++ Assignment07/virtual/Selsort.java
@@ -4,26 +4,34 @@
 import java.util.Random;
 
 class Selsort {
-  public static void main(String[] args) {
-    int count = 10;
-    int[] arr = new int[count];
-    for (int i=0; i<count; i++)
-      arr[i] = rnd.nextInt(1000000);
-    SelectionSort(arr);
-    for (int i=0; i<count; i++)
-      System.out.print(arr[i] + " ");
-    System.out.println();
-  }
+ public static void main(String[] args) {
+  int count = 10;
+  int[] arr = new int[count];
 
-  public static final Random rnd = new Random(); 
+  for (int i = 0; i < count; i++)
+   arr[i] = rnd.nextInt(1000000);
 
-  public static void SelectionSort(int[] arr) {
-    for (int i = 0; i < arr.length; i++) {
-      int least = i;                                      
-      for (int j = i+1; j < arr.length; j++)
-	if (arr[j] < arr[least])
-	  least = j;
-      int tmp = arr[i]; arr[i] = arr[least]; arr[least] = tmp;
-    }
+  SelectionSort(arr);
+
+  for (int i = 0; i < count; i++)
+   System.out.print(arr[i] + " ");
+
+  System.out.println();
+ }
+
+ public static final Random rnd = new Random();
+
+ public static void SelectionSort(int[] arr) {
+  for (int i = 0; i < arr.length; i++) {
+   int least = i;
+
+   for (int j = i + 1; j < arr.length; j++)
+    if (arr[j] < arr[least])
+     least = j;
+   
+   int tmp = arr[i];
+   arr[i] = arr[least];
+   arr[least] = tmp;
   }
+ }
 }
